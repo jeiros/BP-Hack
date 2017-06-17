ad3df2102f121a8f8ba2f5e83b55c7f97b7117c2
joan <pujolweb@hotmail.com>
2017-06-17 20:42:22 +0100

    asdas

--- Data/parse-xml.py
+++ Data/parse-xml.py
@@ -6,7 +6,7 @@ import googlemaps
 from datetime import datetime
 gmaps = googlemaps.Client(key='AIzaSyCqRbZGdjXPN_YUAQbXHPB5760dKXcTq20')
 import pandas as pd
-import xmltodict
+import IPython;
 
 
 def location():
@@ -20,41 +20,34 @@ def location():
 
 def closest_node(point, coordinates):
     coordinates = np.asarray(coordinates)
-    dist_2 = np.sum((coordinates - point)**2, axis=1)
+    dist_2 = np.sum((coordinates.T - point)**2, axis=1)
     return np.argmin(dist_2)
 
 
 
 if __name__ == '__main__':
-    dict_data = {"LA" : [], "LO" : [],"openingHoursMonday" : [],
+    dict_data = {"name" : [],"latitude" : [], "longitude" : [],"openingHoursMonday" : [],
                  "openingHoursTuesday": [], "openingHoursWednesday" : [], "openingHoursThursday":[],
-                 "openingHoursFriday":[], "openingHoursSaturday":[], "openingHoursSunday":[]}
+                 "openingHoursFriday":[], "openingHoursSaturday":[], "openingHoursSunday":[],"keywords" : [],}
     tree = ET.parse('./bpxml.xml')
     root = tree.getroot()
-    print
-    dict(xmltodict.parse('./bpxml.xml')['poiid'])
     #position==location()
     position=np.array([51.5132691, -0.2250459])
-    coords = np.empty((len(root), 2))
-    if False:
-        for i, elem in enumerate(root):
-            for children in elem.getchildren():
-                if 'latitude' in repr(children):
-                    lat = children
-                elif 'longitude' in repr(children):
-                    lon = children
-                elif 'openingHoursMonday' in repr(children):
-                    mon=children
-            coords[i] = (float(lat.text), float(lon.text))
-            #dict_data.append([float(lat.text),float(lat.text),mon,tu,wed,thu,fri,sat,sun])
-    if False:
-        where=closest_node(position,coords)
-        destination = gmaps.reverse_geocode((coords[where][0],coords[where][1]))
-        origin = gmaps.reverse_geocode((position[0], position[1]))
-        now = datetime.now()
-        directions_result = gmaps.directions(origin[0]["formatted_address"], destination[0]["formatted_address"], mode="driving", departure_time=now)
-        print(directions_result[0])
-        print(origin[0]["formatted_address"])
-        print(destination[0]["formatted_address"])
+    for i, elem in enumerate(root):
+        for children in elem.getchildren():
+            for j in dict_data.keys():
+                if j in repr(children):
+                    dict_data[j].append(children.text.rstrip())
+    df=pd.DataFrame.from_dict(dict_data)
+    print(type(dict_data["latitude"]))
+    coords=np.array([df.latitude.astype(float),df.longitude.astype(float)])
+    where=closest_node(position,coords)
+    destination = gmaps.reverse_geocode((coords[0][where],coords[1][where]))
+    origin = gmaps.reverse_geocode((position[0], position[1]))
+    now = datetime.now()
+    directions_result = gmaps.directions(origin[0]["formatted_address"], destination[0]["formatted_address"], mode="driving", departure_time=now)
+    #print(directions_result[0])
+    print(origin[0]["formatted_address"])
+    print(destination[0]["formatted_address"])
+    print(df.iloc[where]["name"],df.iloc[where]["keywords"])
         #print(now)
-        print(data.head())
