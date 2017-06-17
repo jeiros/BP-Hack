cd664532aeb8387081d3e58aa1d8254919819d3c
joan <pujolweb@hotmail.com>
2017-06-17 17:15:43 +0100

    ASDAS

--- Data/parse-xml.py
+++ Data/parse-xml.py
@@ -1,60 +1,60 @@
 import xml.etree.ElementTree as ET
 import numpy as np
-import matplotlib.pyplot as plt
-from mpl_toolkits.basemap import Basemap
+import requests
+import json
+import googlemaps
+from datetime import datetime
+gmaps = googlemaps.Client(key='AIzaSyCqRbZGdjXPN_YUAQbXHPB5760dKXcTq20')
+import pandas as pd
+import xmltodict
 
 
-def draw_world():
-    return Basemap()
+def location():
+    send_url = 'http://freegeoip.net/json'
+    r = requests.get(send_url)
+    j = json.loads(r.text)
+    lat = j['latitude']
+    lon = j['longitude']
+    return np.array([lat,lon])
 
 
-def draw_UK():
-    return Basemap(projection='mill',
-                   resolution='f',
-                   lon_0=-5.23636, lat_0=53.866772,
-                   llcrnrlon=-10.65073, llcrnrlat=49.16209,
-                   urcrnrlon=1.76334, urcrnrlat=60.860699)
+def closest_node(point, coordinates):
+    coordinates = np.asarray(coordinates)
+    dist_2 = np.sum((coordinates - point)**2, axis=1)
+    return np.argmin(dist_2)
+
+
 
 if __name__ == '__main__':
-
+    dict_data = {"LA" : [], "LO" : [],"openingHoursMonday" : [],
+                 "openingHoursTuesday": [], "openingHoursWednesday" : [], "openingHoursThursday":[],
+                 "openingHoursFriday":[], "openingHoursSaturday":[], "openingHoursSunday":[]}
     tree = ET.parse('./bpxml.xml')
     root = tree.getroot()
-
+    print
+    dict(xmltodict.parse('./bpxml.xml')['poiid'])
+    #position==location()
+    position=np.array([51.5132691, -0.2250459])
     coords = np.empty((len(root), 2))
-
-    for i, elem in enumerate(root):
-        for children in elem.getchildren():
-            if 'latitude' in repr(children):
-                lat = children
-            elif 'longitude' in repr(children):
-                lon = children
-        coords[i] = (float(lat.text), float(lon.text))
-
-    np.savetxt('./gas_stations_coords.csv', coords, delimiter=',', fmt='%.4f')
-
-    lat = coords[:, 0]
-    lon = coords[:, 1]
-
-    # World plot
-    f, ax = plt.subplots(figsize=(14, 10))
-    m = draw_world()
-    m.fillcontinents(color='white', lake_color='#eeeeee')
-    m.drawstates(color='lightgray')
-    m.drawcoastlines(color='lightgray')
-    m.drawcountries(color='lightgray')
-    m.drawmapboundary(fill_color='#eeeeee')
-    style = dict(s=5, marker='o', alpha=0.5, zorder=2)
-    m.scatter(lon, lat, latlon=True,
-              color='#00592D', **style)
-    f.savefig('world.png')
-
-    # UK plot
-    f, ax = plt.subplots(figsize=(10, 14))
-    m = draw_UK()
-    m.scatter(lon, lat, latlon=True, s=1, marker=',', color="steelblue", alpha=1)
-    m.fillcontinents(color='white', lake_color='#eeeeee', alpha=.2)
-    m.drawstates(color='lightgray')
-    m.drawcoastlines(color='lightgray')
-    m.drawcountries(color='lightgray')
-    m.drawmapboundary(fill_color='#eeeeee')
-    f.savefig('uk.png')
+    if False:
+        for i, elem in enumerate(root):
+            for children in elem.getchildren():
+                if 'latitude' in repr(children):
+                    lat = children
+                elif 'longitude' in repr(children):
+                    lon = children
+                elif 'openingHoursMonday' in repr(children):
+                    mon=children
+            coords[i] = (float(lat.text), float(lon.text))
+            #dict_data.append([float(lat.text),float(lat.text),mon,tu,wed,thu,fri,sat,sun])
+    if False:
+        where=closest_node(position,coords)
+        destination = gmaps.reverse_geocode((coords[where][0],coords[where][1]))
+        origin = gmaps.reverse_geocode((position[0], position[1]))
+        now = datetime.now()
+        directions_result = gmaps.directions(origin[0]["formatted_address"], destination[0]["formatted_address"], mode="driving", departure_time=now)
+        print(directions_result[0])
+        print(origin[0]["formatted_address"])
+        print(destination[0]["formatted_address"])
+        #print(now)
+        print(data.head())
