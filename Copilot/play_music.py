8f41a4bde80f13d2bb2f62d28ea51f5f65d931eb
jeiros <jeirosz@gmail.com>
2017-06-18 12:39:22 +0100

    best song

--- Copilot/play_music.py
+++ Copilot/play_music.py
@@ -3,5 +3,9 @@ import audio
 
 def play_music():
     pygame.mixer.init()
-    pygame.mixer.music.load("./song.mp3")
+    pygame.mixer.music.load("./winsong.mp3")
     pygame.mixer.music.play()
+
+
+if __name__ == '__main__':
+    play_music()
